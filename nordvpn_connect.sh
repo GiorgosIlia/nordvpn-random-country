736addb6d451a67b7d938e3fc504d0fe10a9cca4
user <giorkos313@gmail.com>
2023-10-18 12:34:18 +0300

    added messages and validatation for nordvpn

--- nordvpn_connect.sh
+++ nordvpn_connect.sh
@@ -3,8 +3,20 @@
 # List of countries
 countries=("United Kingdom" "Germany" "Netherlands" "France" "Sweden" "Switzerland" "Belgium" "Denmark" "Norway" "Poland" "Ireland" "Czech Republic" "Italy" "Spain" "Finland" "Serbia" "Austria" "Slovakia" "Slovenia" "Bulgaria" "Hungary" "Latvia" "Romania" "Portugal" "Luxembourg" "Ukraine" "Greece" "Estonia" "Iceland" "Albania" "Cyprus" "Croatia" "Moldova" "Bosnia and Herzegovina" "Georgia" "North Macedonia" "Lithuania")
 
-# Choose a random country from the list
-selected_country=${countries[RANDOM % ${#countries[@]}]}
+# Check if NordVPN is already connected
+if nordvpn status | grep -q "Status: Connected"; then
+    echo "NordVPN is already connected."
+else
+    # Choose a random country from the list
+    selected_country=${countries[RANDOM % ${#countries[@]}]}
 
-# Command to connect to NordVPN
-nordvpn connect $selected_country
+    # Connect to the random country
+    nordvpn connect $selected_country
+
+    # Check if the connection was successful
+    if nordvpn status | grep -q "Status: Connected"; then
+        echo "NordVPN is now connected to $selected_country."
+    else
+        echo "NordVPN connection failed."
+    fi
+fi
